b2e108b7e764a17215f8e33beb88cee8e4152977
al1aska <seleznevalisha@gmail.com>
2022-03-26 23:59:11 +0400

    Init

---
+++ frontend/src/app/components/main/main.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  selector: 'app-main',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
+})
+export class MainComponent implements OnInit {
+
+  nickname: string = '';
+  password: string = '';
+  email: string = '';
+
+  constructor(public userService: UserService) { }
+
+  ngOnInit(): void {
+  }
+
+  createUser(nickname: string, password: string, email: string){
+    return this.userService.createUser(nickname, password, email).subscribe();
+  }
+
+  get(){
+    return this.userService.getUsers().subscribe();
+  }
+  
+}
