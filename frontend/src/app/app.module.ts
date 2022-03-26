d003a8e054f9fa53cc9f6813c071c47eb850bf56
al1aska <seleznevalisha@gmail.com>
2022-03-26 15:41:27 +0400

    Initial commit

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MainComponent } from './main/main.component';
+import { MainComponent } from './components/main/main.component';
+import { AuthorizationComponent } from './components/authorization/authorization.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent
+    MainComponent,
+    AuthorizationComponent
   ],
   imports: [
     BrowserModule,
