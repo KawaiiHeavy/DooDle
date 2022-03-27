61c7ece26fc45272b6c283c428e15edf6dd313e1
Aleksandr <aleksandr.pronin.2001@gmail.com>
2022-03-27 20:40:44 +0400

    Replaced command line runner onto separate class and made automatic filling roles table

--- src/main/java/com/doodle/DemoApplication.java
+++ src/main/java/com/doodle/DemoApplication.java
@@ -1,13 +1,20 @@
 package com.doodle;
 
+import com.doodle.models.ERole;
+import com.doodle.models.Role;
 import com.doodle.models.User;
+import com.doodle.repostitories.RoleRepository;
 import com.doodle.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -17,21 +24,36 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner runner(UserService userService) {
-        return args -> {
-            User user1 = userService.save(
-                    new User(
-                            UUID.randomUUID(),
-                            "admin",
-                            "admin@gmail.com",
-                            "admin",
-                            "88005553535",
-                            null,
-                            new HashSet<>()
-                    )
-            );
-        };
-    }
+}
 
+@Component
+class DoodleCommandLineRunner implements CommandLineRunner{
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User user1 = userService.save(
+                new User(
+                        UUID.randomUUID(),
+                        "admin1",
+                        "admin1@gmail.com",
+                        "admin1",
+                        "88005553535",
+                        null,
+                        new HashSet<>()
+                )
+        );
+
+        List<Role> roles = new ArrayList<>();
+        roles.add(new Role(ERole.ADMIN));
+        roles.add(new Role(ERole.STUDENT));
+        roles.add(new Role(ERole.TRAINER));
+        roles.add(new Role(ERole.USER));
+        roles.stream().filter(role -> !roleRepository.existsByName(role.getName())).forEach(role -> roleRepository.save(role));
+    }
 }
